6683971b25073dacbc0f28afa94f27effe416d8b
Mariana <mariana.costa@serpro.gov.br>
2017-06-21 16:33:05 -0300

    path

--- src/main/java/rip/Configuration.java
+++ src/main/java/rip/Configuration.java
@@ -2,14 +2,13 @@ package rip;
 
 import javax.servlet.ServletContext;
 
-import org.apache.commons.lang3.StringUtils;
-
 public class Configuration {
 
 	public static String getDataPath(ServletContext context) {
-		String warPath = context.getRealPath("/uploads");
-		String path = StringUtils.isEmpty(System.getenv("OPENSHIFT_DATA_DIR")) ? warPath
-				: System.getenv("OPENSHIFT_DATA_DIR");
-		return path;
+		// String warPath = context.getRealPath("/uploads");
+		// String path =
+		// StringUtils.isEmpty(System.getenv("OPENSHIFT_DATA_DIR")) ? warPath
+		// : System.getenv("OPENSHIFT_DATA_DIR");
+		return context.getRealPath("/uploads");
 	}
 }
